15f81942931b93bebbb7e6d7ee1b31bf86467683
abedjamous <jamous.hameed94@gmail.com>
2021-07-23 12:35:57 -0500

    second commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { EventEmitter, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
@@ -7,14 +8,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
   recipes: Recipe[] =[
     new Recipe('A Test Recipe', 'This is simply a test', 'https://vilas.extension.wisc.edu/files/2013/12/Recipes-Title.png'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://vilas.extension.wisc.edu/files/2013/12/Recipes-Title.png')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://vilas.extension.wisc.edu/files/2013/12/Recipes-Title.png')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ ingredients: Ingredient[]= [
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 }
