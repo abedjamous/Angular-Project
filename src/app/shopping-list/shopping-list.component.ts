c5f44b4961e14cf46bed629306ac6287b2f3efc3
Abed Jamous <abedjamous@Abeds-MacBook-Pro.local>
2021-09-12 20:25:35 -0500

    added form and make clear and delete button works

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,26 +1,36 @@
 import { ShoppingListService } from './shopping-list.service';
 import { Ingredient } from './../shared/ingredient.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
-ingredients: Ingredient[]
+export class ShoppingListComponent implements OnInit, OnDestroy {
+ingredients: Ingredient[];
+private igChangeSub: Subscription;
 
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit(){
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
+    this.igChangeSub = this.slService.ingredientsChanged
     .subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
+  }
+
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
+  ngOnDestroy() {
+    this.igChangeSub.unsubscribe();
   }
 
 }
