6799d80a8e1654b09dd87f34191e934b029e8064
Abed Jamous <abedjamous@Abeds-MBP.lan>
2021-10-04 11:32:57 -0500

    add login component and attached it with http

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,67 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import {throwError} from 'rxjs';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  
+
+  constructor(private http: HttpClient) { }
+ 
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA8714liugV38RB3y6q8B3kQnl7JvVyOGE',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+      ).pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+     return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA8714liugV38RB3y6q8B3kQnl7JvVyOGE',
+       {
+        email: email,
+        password: password,
+        returnSecureToken: true
+       }
+      ).pipe(catchError(this.handleError));
+      
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email exists already!';
+            break;
+            case 'EMAIL_NOT_FOUND':
+              errorMessage = 'This Email does not exist.'
+              break;
+              case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct!'
+              break;
+        }
+        return throwError(errorMessage);
+      }
+  
+
+}
